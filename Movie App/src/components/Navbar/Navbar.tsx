16cd1e3e6cb1e1cca5192893377bb3baa83adfd1
Addel <sigmamediacode@gmail.com>
2025-01-28 17:36:31 -0400

    Componente Navbar con MUI añadida
    pendientes a resolver, flexbox y estilos

---
+++ Movie App/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import { AppBar, Box, Button, IconButton, InputBase, Toolbar, Typography } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: "1" }}>
+          Movie App
+        </Typography>
+        <Box sx={{display:"flex", alignItems: "center"}}>
+            <InputBase
+            placeholder="Buscar..."
+            sx={{ml: 1, flex: 1, bgcolor: "white", borderRadius: 1, px: 2}}
+            />
+            <IconButton>
+                <SearchIcon />
+            </IconButton>
+        </Box>
+        <Button color="inherit">Inicio</Button>
+        <Button color="inherit">Películas</Button>
+        <Button color="inherit">Series</Button>
+        <Button color="inherit">Categorías</Button>
+        <IconButton aria-label="login">
+            <AccountCircleIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
