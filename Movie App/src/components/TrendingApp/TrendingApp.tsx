a8ec1e1557ec81572eb32caf72051dbda3205aa5
Addel <sigmamediacode@gmail.com>
2025-02-27 00:17:22 -0400

    aplicacion de mui para titulo y poster de las peliculas trending

--- Movie App/src/components/TrendingApp/TrendingApp.tsx
+++ Movie App/src/components/TrendingApp/TrendingApp.tsx
@@ -2,42 +2,62 @@
 import { useEffect, useState } from "react";
 // 2 importar getTrendingMovies
 import { getTrendingMovies } from "../../services/tmdbService";
+import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
 // Definir la interfaz para describir la estructura de los objetos de película
 interface Movie {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
+  backdrop_path: string;
 }
 
 // 3 crear el componente TrendingApp
 function TrendingApp() {
-    // 4 crear un estado para las películas movies
-    const  [movies, setMovies] = useState<Movie[]>([]);
+  // 4 crear un estado para las películas movies
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    // 5 usar useEffect para obtener las películas trending, usando async con una función fetchMovies
-    useEffect(() =>{
-        async function fetchMovies() {
-            // 6 constante data y await getTrendingMovies
-            const data = await getTrendingMovies();
-            console.log('data recibida:', data);
-            // 7 setMovies con data
-            setMovies(data || []);
-        }
-        // 8 llamar fetchMovies en useEffect
-        fetchMovies();
-    }, []);
+  // 5 usar useEffect para obtener las películas trending, usando async con una función fetchMovies
+  useEffect(() => {
+    async function fetchMovies() {
+      // 6 constante data y await getTrendingMovies
+      const data = await getTrendingMovies();
+      console.log("data recibida:", data);
+      // 7 setMovies con data
+      setMovies(data || []);
+    }
+    // 8 llamar fetchMovies en useEffect
+    fetchMovies();
+  }, []);
 
-    // 9 return un div con un h1, un ul, y un map de movies, con un li y un key
-    return (
-        <div>
-            <h1>Trending Movies</h1>
-            <ul>
-                {movies.map((movie) => (
-                    <li key={movie.id}>{movie.title}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  // 9 return un div con un h1, un ul, y un map de movies, con un li y un key
+  return (
+    <>
+      <h1>Trending Movies</h1>
+
+      {movies.map((movie) => (
+        <Card key={movie.id} style={{ maxWidth: 340 }}>
+          <CardMedia>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt="movie"
+              //   style={{ height: 300 }}
+            ></img>
+          </CardMedia>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {movie.title}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
 }
 
 // 10 exportar TrendingApp
 export default TrendingApp;
+// modificar el return del componente TrendingApp card para mostrar la información de la película (titulo e imagen)
+// 1 importar Card, CardContent, CardMedia, Typography
+// 2 componente card para rodear el componente card
+// 3 CardMedia para la imagen de la película
+// 4 CardContent para el título de la película
+// 5 Typography para el texto del título (Opcional)
